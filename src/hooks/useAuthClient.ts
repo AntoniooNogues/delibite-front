903e47dcf18c55b61733e5b8c6c12b8fd0d20361
Rodrigo <rjaencobos@safareyes.es>
2025-02-14 17:15:53 +0100

    ✨ Creación del manejo de errores para el usuario.

---
+++ src/hooks/useAuthClient.ts
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { jwtDecode } from "jwt-decode";
+import Cookies from "js-cookie";
+import { ErrorPropsInterface } from "@/pages/ErrorPropsInterface";
+
+interface DecodedToken {
+    roles: string[];
+    username: string;
+}
+
+export const useAuth = () => {
+    const [token, setDecodedToken] = useState<DecodedToken | null>(null);
+    const [error, setError] = useState<ErrorPropsInterface | null>(null);
+    const router = useRouter();
+
+    useEffect(() => {
+        const tokenUsuario = Cookies.get("token");
+
+        if (!tokenUsuario) {
+            setError({
+                errorCode: "401",
+                title: "Acceso denegado",
+                message: "Debes iniciar sesión para acceder.",
+                url: "/login",
+            });
+            router.push("/login");
+            return;
+        }
+
+        try {
+            const tokenDescodificado = jwtDecode<DecodedToken>(tokenUsuario);
+            setDecodedToken(tokenDescodificado);
+
+            if (!tokenDescodificado.roles.includes("ROLE_USUARIO")) {
+                setError({
+                    errorCode: "403",
+                    title: "Acceso denegado",
+                    message: "No tienes permisos para acceder a esta página.",
+                    url: "/login",
+                });
+                router.push("/login");
+            }
+        } catch (error) {
+            console.error("Error decoding token:", error);
+            setError({
+                errorCode: "401",
+                title: "Acceso denegado",
+                message: "Debes iniciar sesión para acceder.",
+                url: "/login",
+            });
+            router.push("/login");
+        }
+    }, [router]);
+
+    return { token, error };
+};
