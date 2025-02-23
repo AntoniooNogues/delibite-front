8bea480c40f80cc883331816baafc3b8e7ed68ff
Antonio Nogues <anoguesgomez@safareyes.es>
2025-02-23 14:14:49 +0100

    ✨añadir interfaces para packs

--- src/interfaces/Administracion-Interfaces.ts
+++ src/interfaces/Administracion-Interfaces.ts
@@ -5,6 +5,22 @@ export interface HomeInterface {
     usuarios_activos_ultimos_7_dias: number;
 }
 
-export interface Pack {
 
+export interface cargarPlatosPack{
+    platos: Plato[];
+}
+
+export interface Pack {
+    pack_id: number;
+    nombre: string;
+    descripcion: string;
+    precio: number;
+    visibilidad: boolean;
+    platos: Plato[];
+}
+
+export interface Plato {
+    id: number;
+    nombre: string;
+    precio: number;
 }
