ca0758ef622068b004e4b7224dd791ffbd486032
Antonio Nogues <anoguesgomez@safareyes.es>
2025-02-26 19:49:48 +0100

    🐛💄arreglar bug y modificar estilos

--- src/app/verificar-cuenta/page.tsx
+++ src/app/verificar-cuenta/page.tsx
@@ -7,23 +7,17 @@ import { Notificaciones } from '@/interfaces/Notificaciones';
 import axios from "axios";
 import NavbarReducido from "@/components/NavbarReducido";
 import type { VerificarCuenta } from "@/interfaces/VerificarCuenta";
-import { BackwardIcon } from "@heroicons/react/20/solid";
+import {ArrowLeftIcon, BackwardIcon} from "@heroicons/react/20/solid";
 import Cookies from "js-cookie";
 import ErrorPage from "@/pages/[_error]";
+import Navbar from "@/components/Navbar";
 
 export default function VerificarCuenta() {
     const router = useRouter();
     const [propiedadesCuenta, setPropiedades] = useState<VerificarCuenta>();
     const [notificacion, setNotificacion] = useState<Notificaciones | undefined>(undefined);
-    const [existeToken, setExisteToken] = useState(false);
     const token = Cookies.get("token");
 
-    useEffect(() => {
-        if (token) {
-            setExisteToken(true);
-        }
-
-    }, [token, router]);
 
     const [errorInput, setErrorInput] = useState(false);
     const [step, setStep] = useState(1);
@@ -60,15 +54,13 @@ export default function VerificarCuenta() {
         }
     };
 
-    const handleVerify = () => {
-        setPropiedades({ email: email, codigo: code });
-        verificarCuenta();
-    };
 
-    const verificarCuenta = async () => {
+    const handleVerify = async () => {
         try {
-            console.log(propiedadesCuenta);
-            const respuesta = await axiosClient.post("/verificar-email", propiedadesCuenta);
+            const data = { "email": email, "codigo": code };
+            console.log(data);
+            const respuesta = await axiosClient.post("/verificar-email", data);
+            console.log(respuesta);
             setNotificacion({ titulo: respuesta.data.titulo, mensaje: respuesta.data.mensaje, code: respuesta.data.code, tipo: respuesta.data.tipo });
             setTimeout(() => {
                 router.push("/login");
@@ -82,83 +74,77 @@ export default function VerificarCuenta() {
 
     return (
         <>
-            {existeToken ? (
-                <ErrorPage errorCode="403" title="No tienes permisos para acceder a esta pagina" message="Ya has verificado la cuenta." url="/" />
-            ) : (
-                <div className="h-screen flex flex-col w-screen bg-(--gris-registro)">
-                    <NavbarReducido />
-                    <div className="h-screen flex items-center justify-center bg-oxley-50 p-4">
-                        <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-lg">
-                            {step === 1 ? (
-                                <>
-                                    <h2 className="text-3xl font-bold text-center text-primary-dark mb-2">
+            <div className="h-screen flex flex-col w-screen bg-(--gris-registro)">
+                <div className="absolute w-full">
+                    <Navbar />
+                </div>
+                <div className="h-screen flex items-center justify-center p-4">
+                    <div className="p-8 rounded-lg w-full max-w-xl min-h-96 flex flex-col gap-8  items-center justify-center">
+                        {step === 1 ? (
+                            <>
+                                <header className="space-y-3 flex flex-col">
+                                    <h2 className="text-3xl font-bold text-center text-primary-dark">
                                         Verificación de Cuenta
                                     </h2>
                                     <p className="text-center text-oxley-500">
                                         Introduce tu correo para recibir el código
                                     </p>
-                                    <div className="mt-6">
-                                        <label className="block text-base font-medium text-oxley-700">
-                                            Correo Electrónico
-                                        </label>
-                                        <input
-                                            type="email"
-                                            value={email}
-                                            onChange={(e) => setEmail(e.target.value)}
-                                            className={`mt-2 w-full border-2 ${errorInput ? 'border-(--danger-600)' : 'border-black'} p-3 rounded-lg`}
-                                        />
-                                    </div>
-                                    <button
-                                        onClick={handleNextStep}
-                                        className="mt-6 w-full px-5 py-3 bg-(--oxley-500) text-white rounded-lg hover:bg-(--primary-dark) hover:scale-105 active:scale-95 transition-transform"
-                                    >
-                                        Enviar Código
+                                </header>
+                                <div className="space-y-6 flex flex-col w-full">
+                                    <input
+                                        type="email"
+                                        placeholder={"Correo Electrónico"}
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        className={`mt-2 w-full border border-(--oxley-300) p-3 rounded-lg text-center tracking-widest`}
+                                    />
+                                </div>
+                                <button
+                                    onClick={handleNextStep}
+                                    className=" w-full px-5 py-3 bg-(--oxley-500) text-white rounded-lg hover:bg-(--primary-dark) hover:scale-105 active:scale-95 transition-transform"
+                                >
+                                    Enviar Código
+                                </button>
+                            </>
+                        ) : (
+                            <>
+                                <div className="flex items-center justify-between w-full">
+                                    <button onClick={() => setStep(1)} className="w-12 h-12 hover:scale-105 active:scale-95 transition-transform ">
+                                        <ArrowLeftIcon className="h-fit w-fit text-gray-800 bg-white p-3 rounded-full sha" />
                                     </button>
-                                </>
-                            ) : (
-                                <>
-                                    <div className="flex items-center justify-between">
-                                        <button onClick={() => setStep(1)} className="placeholder:scale-105 active:scale-95 transition-transform">
-                                            <BackwardIcon className="h-6 w-6 text-gray-800" />
-                                        </button>
-                                    </div>
-                                    <div>
-                                        <h2 className="text-3xl font-bold text-center text-primary-dark mb-2 flex-grow">
-                                            Verificar Código
-                                        </h2>
-                                    </div>
-                                    <p className="text-center text-oxley-500">
-                                        Introduce el código enviado a {email}
-                                    </p>
-                                    <div className="mt-6">
-                                        <label className="block text-base font-medium text-oxley-700">
-                                            Código de Verificación
-                                        </label>
-                                        <input
-                                            type="text"
-                                            value={code}
-                                            onChange={(e) => setCode(e.target.value)}
-                                            className="mt-2 w-full border border-oxley-300 p-3 rounded-lg text-center tracking-widest "
-                                        />
-                                    </div>
-                                    <button
-                                        onClick={handleVerify}
-                                        className="mt-6 w-full px-5 py-3 bg-(--oxley-500) text-white rounded-lg hover:bg-(--primary-dark) hover:scale-105 active:scale-95 transition-transform"
-                                    >
-                                        Verificar
-                                    </button>
-                                </>
-                            )}
-                        </div>
+                                    <h2 className="text-3xl font-bold text-center text-primary-dark mx-auto">
+                                        Verificar Código
+                                    </h2>
+                                </div>
+                                <p className="text-center text-oxley-500">
+                                    Introduce el código enviado a {email}
+                                </p>
+                                <div className="w-full">
+                                    <input
+                                        type="text"
+                                        placeholder={"Código de Verificación"}
+                                        value={code}
+                                        onChange={(e) => setCode(e.target.value)}
+                                        className="mt-2 w-full border border-(--oxley-300) p-3 rounded-lg text-center tracking-widest"
+                                    />
+                                </div>
+                                <button
+                                    onClick={handleVerify}
+                                    className="w-full px-5 py-3 bg-(--oxley-500) text-white rounded-lg hover:bg-(--primary-dark) hover:scale-105 active:scale-95 transition-transform"
+                                >
+                                    Verificar
+                                </button>
+                            </>
+                        )}
                     </div>
-                    {notificacion && (
-                        <NotificacionComponent
-                            Notificaciones={notificacion}
-                            onClose={() => setNotificacion(undefined)}
-                        />
-                    )}
                 </div>
-            )}
+                {notificacion && (
+                    <NotificacionComponent
+                        Notificaciones={notificacion}
+                        onClose={() => setNotificacion(undefined)}
+                    />
+                )}
+            </div>
         </>
     );
 }
