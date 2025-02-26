cced936fb502db3c39f1329391e862d2fca003f6
Antonio Nogues <anoguesgomez@safareyes.es>
2025-02-26 20:48:35 +0100

    🚀 v0.5

---
+++ src/app/perfil/facturas/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react';
+
+const FacturaPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Facturas</h1>
+        </div>
+    );
+};
+
+export default FacturaPage;
---
+++ src/app/perfil/suscripcion/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react';
+
+const SuscripcionPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Suscripcion</h1>
+        </div>
+    );
+};
+
+export default SuscripcionPage;
