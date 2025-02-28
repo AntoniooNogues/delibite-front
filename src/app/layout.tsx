c022f5c86a65945301b93fd344b0dffb26f1af73
Antonio Nogues <anoguesgomez@safareyes.es>
2025-02-28 11:52:56 +0100

    🎨mejorar estructura de layaout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,24 +2,17 @@ import type { Metadata } from "next";
 import "./globals.css";
 import React from "react";
 
-
-
 export const metadata: Metadata = {
     title: "Delibite",
     description: "Creado por Antonio Nogues y Rodrigo Jaén",
 };
 
-export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="es">
         <head>
             <link rel="icon" href="/logo.jpg"/>
             <link rel="preconnect" href="https://fonts.gstatic.com"/>
-            <title>Delibite</title>
         </head>
         <body className="antialiased">
         {children}
