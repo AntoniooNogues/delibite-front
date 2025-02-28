55788335bf1c2384ad1ed7288b3c4c18681fd894
Antonio Nogues <anoguesgomez@safareyes.es>
2025-02-28 10:51:30 +0100

    🎨💄Cambios de lógica y estilos en manejo de carrito

--- src/app/detallesCarrito/page.tsx
+++ src/app/detallesCarrito/page.tsx
@@ -15,7 +15,7 @@ import {Notificaciones} from "@/interfaces/Notificaciones";
 
 
 export default function DetallesCarrito() {
-    const [carrito, setCarrito] = useState<{ [key: number]: { nombre: string, precio: number, cantidad: number } }>({});
+    const [carrito, setCarrito] = useState<{ [key: number]: { nombre: string, precio: number, cantidad: number, url: string } }>({});
     const notificacion = useTokenExpirado();
     const [notificacionState, setNotificacionState] = useState<Notificaciones | null>(null);
 
@@ -84,7 +84,11 @@ export default function DetallesCarrito() {
     const totalCarrito = Object.values(carrito).reduce((total, { precio, cantidad }) => total + (precio * cantidad), 0);
     const [mostrarFormularioPago, setMostrarFormularioPago] = useState(false);
     const handleFinalizarCompra = () => {
-        setMostrarFormularioPago(true);
+        if (totalCarrito === 0) {
+            setNotificacionState({ titulo: "Error", mensaje: "No hay productos en el carrito para finalizar la compra", code: 500, tipo: "error" });
+        }else{
+            setMostrarFormularioPago(true);
+        }
     };
     const [pais, setPais] = useState("España");
     const calcularEnvio = () => {
@@ -100,21 +104,23 @@ export default function DetallesCarrito() {
         }
     };
     const precioEnvio = calcularEnvio();
-    const totalConEnvio = totalCarrito + precioEnvio;
+    const totalConEnvio = totalCarrito > 0 ? totalCarrito + precioEnvio : 0;
     return (
         <div>
-            <Navbar></Navbar>
-            <motion.div initial={{ y: -50, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.2 }}>
+            <div className="relative w-full z-50">
+                <Navbar />
+            </div>
+            <motion.div initial={{ y: -50, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.2 }} className="min-h-screen">
                 {/* Mostrar productos */}
-                <div className="mt-8 mx-auto w-5/6 flex space-x-6 mb-12">
-                    <div className="w-2/3 bg-white p-6 rounded-2xl shadow-lg">
+                <div className="mt-8 mx-auto w-5/6 flex space-x-6 mb-12 h-full min-h-[600px] max-h-[600px]">
+                    <div className="w-2/3 bg-white p-6 rounded-2xl shadow-lg overflow-y-auto">
                         <span className="text-2xl">
                             Mi carrito
                         </span>
                         <hr/>
                         <div className="space-y-2 mt-2 ">
                             {Object.keys(carrito).length > 0 ? (
-                                Object.entries(carrito).map(([id, { nombre, precio, cantidad }], i) => {
+                                Object.entries(carrito).map(([id, { nombre, precio, cantidad, url }], i) => {
                                     return (
                                         <div
                                             key={id}
@@ -124,7 +130,7 @@ export default function DetallesCarrito() {
                                         >
                                             <div className="flex justify-between items-center space-x-4" style={{ flexShrink: 0 }}>
                                                 <div>
-                                                    <Image src={"/catalogo/papas_arrugas.webp"} alt={"Imagen producto"} className="rounded-lg" width={140} height={70}></Image>
+                                                    <Image src={url} alt={"Imagen producto"} className="rounded-lg" width={140} height={70} />
                                                 </div>
                                                 <div className="flex flex-1 items-center space-x-4">
                                                     <p className="text-lg font-semibold">{nombre}</p>
@@ -134,9 +140,8 @@ export default function DetallesCarrito() {
                                                 </div>
                                                 <div className="min-w-2 shrink-0 ">
                                                     <CantidadControl
-                                                        itemId={parseInt(id)}
                                                         cantidadInicial={cantidad}
-                                                        handleCantidadChange={(itemId, value) => handleCantidadChange(itemId, value)}
+                                                        handleCantidadChange={(value: number) => handleCantidadChange(parseInt(id), value)}
                                                         width={30}
                                                         height={30}
                                                     />
@@ -183,91 +188,61 @@ export default function DetallesCarrito() {
                                 </DialogContentText>
                             </DialogContent>
                             <DialogActions>
-                                <Button
-                                    onClick={handleClose}
-                                    sx={{
-                                        backgroundColor: 'var(--verde-azulado)',
-                                        color: 'white',
-                                        '&:hover': {
-                                            backgroundColor: 'var(--primary-dark)',
-                                            transform: 'scale(1.05)'
-                                        },
-                                        '&:active': {
-                                            backgroundColor: 'var(--oxley-800)',
-                                            transform: 'scale(0.95)',
-                                        },
-                                        padding: '10px 20px',
-                                        borderRadius: '5px',
-                                        boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.2)',
-                                    }}
-                                >
+                                <button onClick={handleClose} className="mt-8 w-1/2 bg-(--primary-dark) text-white py-3 rounded-full hover:scale-105 active:scale-95 text-lg">
                                     Cancelar
-                                </Button>
-                                <Button
-                                    onClick={() => selectedId !== null && borrarProducto(selectedId)}
-                                    autoFocus
-                                    sx={{
-                                        backgroundColor: 'var(--danger-400)',
-                                        color: 'white',
-                                        '&:hover': {
-                                            backgroundColor: 'var(--danger-600)',
-                                            transform: 'scale(1.05)'
-                                        },
-                                        '&:active': {
-                                            backgroundColor: 'var(--danger-800)',
-                                            transform: 'scale(0.95)',
-                                        },
-                                        padding: '10px 20px',
-                                        borderRadius: '5px',
-                                        boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.2)',
-                                    }}
-                                >
+                                </button>
+
+                                <button onClick={() => selectedId !== null && borrarProducto(selectedId)} className="mt-8 w-1/2 bg-(--danger-400) text-white py-3 rounded-full hover:scale-105 active:scale-95 text-lg">
                                     Eliminar
-                                </Button>
+                                </button>
                             </DialogActions>
                         </Dialog>
                     </div>
-                    <div className="w-1/3 bg-white p-6 rounded-2xl shadow-lg max-h-86">
-                        <span className="text-2xl">
+                    <div className="w-1/3 bg-white p-6 rounded-2xl shadow-lg flex flex-col justify-between overflow-y-auto">
+                        <div>
+                            <span className="text-2xl">
                             Resumen del pedido
                         </span>
-                        <hr/>
-                        <div className="mt-4">
-                            <div className="flex text-lg justify-between">
-                                <p>Subtotal: </p>
-                                <p>
-                                    {(totalCarrito - (totalCarrito * 0.21)).toFixed(2)}
+                            <hr/>
+                            <div className="mt-4">
+                                <div className="flex text-lg justify-between overflow-y-auto scroll">
+                                    <p>Subtotal: </p>
+                                    <p>
+                                        {(totalCarrito - (totalCarrito * 0.21)).toFixed(2)}
+                                    </p>
+                                </div>
+                                <div className="flex text-lg justify-between">
+                                    <p>Impuestos: </p>
+                                    <p> {(totalCarrito * 0.21).toFixed(2)} </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="justify-between">
+                            <div className="flex items-center justify-between">
+                                <p
+                                    className="underline hover:scale-105 active:scale-95"
+                                >
+                                    Calcular envío
                                 </p>
+                                <select value={pais} onChange={(e) => setPais(e.target.value)}
+                                        className="mt-2 p-2 border rounded">
+                                    <option value="España">España</option>
+                                    <option value="Portugal">Portugal</option>
+                                    <option value="Francia">Francia</option>
+                                </select>
                             </div>
-                            <div className="flex text-lg justify-between">
-                                <p>Tax: </p>
-                                <p> {(totalCarrito * 0.21).toFixed(2)} </p>
+                            <div className="flex justify-between text-xl mt-8">
+                                <p className="font-bold">Total: </p>
+                                <p className="font-bold">{(totalConEnvio).toFixed(2)}</p>
                             </div>
-                        </div>
-                        <div className="flex items-center justify-between">
-                            <p
-                                className="underline hover:scale-105 active:scale-95"
-                            >
-                                Calcular envío
-                            </p>
-                            <select value={pais} onChange={(e) => setPais(e.target.value)}
-                                    className="mt-2 p-2 border rounded">
-                                <option value="España">España</option>
-                                <option value="Portugal">Portugal</option>
-                                <option value="Francia">Francia</option>
-                            </select>
-                        </div>
-                        <div className="flex justify-between text-xl mt-8">
-                            <p className="font-bold">Total: </p>
-                            <p className="font-bold">{(totalConEnvio).toFixed(2)}</p>
-                        </div>
-                        <div className="flex items-center justify-center w-full mt-12">
-                            <button
-                                onClick={handleFinalizarCompra}
-                                className="px-4 py-3 bg-(--oxley-300) hover:bg-(--verde-azulado)
+                            <div className="flex items-center justify-center w-full mt-12">
+                                <button
+                                    onClick={handleFinalizarCompra}
+                                    className="px-4 py-3 bg-(--oxley-300) hover:bg-(--verde-azulado)
                             active:bg-(--oxley-500) transition transform hover:scale-105 active:scale-95 w-full rounded-2xl font-bold">
-                                Finalizar compra
-                            </button>
+                                    Finalizar compra
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
