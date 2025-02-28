7c4fec0be146a61fa94896caf04dc28456b41ffa
Antonio Nogues <anoguesgomez@safareyes.es>
2025-02-28 11:51:41 +0100

    🚚cambiar nombre a pagina global de errores

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -3,15 +3,17 @@ import { useRouter } from 'next/navigation';
 import '/src/app/administracion/administracion.css';
 import { ErrorPropsInterface } from "@/interfaces/ErrorPropsInterface";
 
-const ErrorPage: React.FC<ErrorPropsInterface> = ({ errorCode, title, message, url, color, textoBoton }) => {
+const ErrorPage: React.FC<ErrorPropsInterface> = ({ color, url, errorCode, title, message, textoBoton }) => {
     const router = useRouter();
-    let fondo: string = "";
-    if (color > 3 ||color < 1 || color === 1) {
-        fondo = "#EFEFEF";
-    }
-    if (color === 2) {
-        fondo = "#000";
-    }
+    const fondo = color === 2 ? "#000" : "#EFEFEF";
+
+    const handleClick = () => {
+        if (url) {
+            router.push(url);
+        } else {
+            router.push('/');
+        }
+    };
 
     return (
         <div className={`min-h-screen flex flex-col items-center justify-center`} style={{ backgroundColor: fondo }}>
@@ -20,7 +22,7 @@ const ErrorPage: React.FC<ErrorPropsInterface> = ({ errorCode, title, message, u
                 <h1 className="text-6xl font-bold text-(--oxley-500) text-center">{title}</h1>
                 <p className="text-2xl text-(--verde-azulado) mt-4 text-center">{message}</p>
                 <button
-                    onClick={() => router.push(url)}
+                    onClick={handleClick}
                     className="mt-6 px-4 py-2 bg-(--primary-dark) text-white rounded-lg hover:scale-105 active:scale-95 shadow-2xl"
                 >
                     {textoBoton}
